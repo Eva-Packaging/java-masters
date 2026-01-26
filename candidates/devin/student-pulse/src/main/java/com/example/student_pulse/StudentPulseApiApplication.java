7bcd09c7b8214dc2442f96d574b8cda82a52439f
Devin Catuns <devincatuns1@gmail.com>
2026-01-26 14:18:42 -0500

    feat: created project

---
+++ candidates/devin/student-pulse/src/main/java/com/example/student_pulse/StudentPulseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_pulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPulseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPulseApiApplication.class, args);
+	}
+
+}
